f72a0d052f8dbd797f844a2767b78e49cad7db00
armandios19 <fooseaov@gmail.com>
2022-07-11 22:00:11 -0500

    claculando p value

--- scripts/calculando_p_value.R
+++ scripts/calculando_p_value.R
@@ -1,1 +1,24 @@
 ### calculando p value
+
+
+# ejemplo con prueba t
+
+t.test(
+  x           = smoker,
+  y           = nonsmoker,
+  alternative = "two.sided",
+  mu          = 0,
+  var.equal   = TRUE,
+  conf.level  = 0.95
+)
+
+
+les <- c(4,17)
+AR <- c(6,13)
+
+t.test(x = les,
+       y = AR,
+       altenative = "two.sided",
+       mu = 0,
+       var.equal = TRUE,
+       conf.level = 0.95)
