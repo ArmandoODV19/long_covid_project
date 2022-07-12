0b643122360e63e72b43900b8c42324596c39ddf
armandios19 <fooseaov@gmail.com>
2022-07-12 01:25:22 -0500

    script forest plot les vs ar

--- scripts/grafos.R
+++ scripts/grafos.R
@@ -1,1 +1,16 @@
 ### grafos para forest plot
+
+forest_les_ar <- read_xlsx("data/forest_les_ar.xlsx")
+
+
+ggplot(forest_les_ar, aes(y = forcats::fct_rev(f = forcats::fct_inorder(f = secuela)))) +
+  geom_vline(xintercept = 1, linetype = "dashed") +
+  geom_point(mapping = aes(x = riesgo_relativo, color = "orange")) +
+  geom_errorbarh(mapping = aes(xmin = bajo,
+                               xmax = alto)) +
+  theme_light() +
+  scale_x_discrete(limits=c(0,1,2,3,4,5,6,7,8,9,10,11)) +
+  labs(x = "Relative risk",
+       y = "") +
+  theme(legend.position = "none") +
+  labs(y = NULL)
